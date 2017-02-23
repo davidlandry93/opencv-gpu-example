54a306b1767140a1e4a04af9b9d38c8fa21f015d
David Landry <davidlandry93@gmail.com>
2017-02-23 14:55:41 -0500

    Added gain printout

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 
 using namespace std;
 #include <opencv2/opencv.hpp>
@@ -15,13 +16,28 @@ int main (int argc, char* argv[])
             cv::cuda::GpuMat dst, src;
             src.upload(src_host);
 
+            auto start = chrono::steady_clock::now();
             cv::cuda::threshold(src, dst, 100.0, 255.0, CV_THRESH_BINARY);
+            auto accelerated_duration = chrono::steady_clock::now() - start;
 
             cv::Mat result_host;
             dst.download(result_host);
             // cv::imshow("Input", src_host);
             // cv::imshow("Result", result_host);
             // cv::waitKey();
+
+            cv::Mat dst_host;
+
+            start = chrono::steady_clock::now();
+            cv::threshold(src_host, dst_host, 100.0, 255.0, CV_THRESH_BINARY);
+            auto host_duration = chrono::steady_clock::now() - start;
+
+
+            cout << "=== Thresholding ===" << endl;
+            cout << "Host time: " << chrono::duration<float,milli>(host_duration).count() << " ms." << endl;
+            cout << "Device time: " << chrono::duration<float,milli>(accelerated_duration).count() << " ms." << endl;
+            cout << "Speedup: " << chrono::duration<float,milli>(host_duration).count() / chrono::duration<float, milli>(accelerated_duration).count()  << "x." << endl;
+
         }
     catch(const cv::Exception& ex)
         {
